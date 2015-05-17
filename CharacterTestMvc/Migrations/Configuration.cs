019f98d2cce67c3072bb0e25dfc9dbaca47d03a7
Corbin Murrow <corbin.murrow@gmail.com>
2015-05-16 23:50:46 -0500

    Testing my patience...

--- CharacterTestMvc/Migrations/Configuration.cs
+++ CharacterTestMvc/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace CharacterTestMvc.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "CharacterTestMvc.Models.CharacterDb";
         }
 
